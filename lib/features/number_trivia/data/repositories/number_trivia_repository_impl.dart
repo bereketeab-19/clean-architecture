115f58ae3328deb50d9a85576bb42eac59495716
bereketeab-19 <bereketeabbelete@gmail.com>
2025-04-11 11:09:40 +0300

    network info and test done

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -3,7 +3,7 @@ import 'package:clean_architecture/core/error/exceptions.dart';
 import 'package:clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:dartz/dartz.dart';
 
-import 'package:clean_architecture/core/platform/network_info.dart';
+import 'package:clean_architecture/core/network/network_info.dart';
 import 'package:clean_architecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:clean_architecture/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 
